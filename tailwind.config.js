6851f8be2d9765179a64a57d87f3520605d5d6c3
KC <aswinkcmji@gmail.com>
2025-01-03 22:39:18 +0530

    add new psyfos code

---
+++ tailwind.config.js
@@ -0,0 +1,38 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.{html,js}"],
+  theme: {
+    container: {
+      padding: {
+        DEFAULT: '15px'
+      }
+    },
+    screens: {
+      xs: '420px',
+      sm: '640px',
+      md: '768px',
+      lg: '960px',
+      xl: '1330px',
+    },
+    extend: {
+      colors: {
+        primary: "#02a9a4",
+        secondary: "#2e244e",
+        accent: {
+          DEFAULT: '#1cbccf',
+          secondary: '#18abbc',
+          teritiary: '#90c6cd'
+        },
+        grey: '#e8f0f1',
+      },
+      fontFamily: {
+        primary: 'Poppins'
+      },
+      boxShadow: {
+        custom1: '0px 2px 40px 0px rgba(8, 70, 78, 0.08)',
+        custom2: '0px 0px 30px 0px rgba(8, 73, 81, 0.06)',
+      },
+    },
+  },
+  plugins: [],
+}
